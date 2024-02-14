6eb03d83b077aa5926a2a48bf51ef79b02225a8b
yulua <yulua@DESKTOP-CA8UJJN>
2024-02-14 16:30:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+float operation(string oper, float a, float b) {
+	if (oper == "*") {
+		return a * b;
+	}
+	else if (oper == "/") {
+		return a / b;
+	}
+	else if (oper == "+") {
+		return a + b;
+	}
+	else if (oper == "-") {
+		return a - b;
+	}
+	else {
+		cout << "";
+	}
+}
+
+int main()
+{
+	float a;
+	float b;
+	cout << "Введите переменные a и b\n";
+	cin >> a >> b;
+
+	cout << "Выберите операцию (* / + -): ";
+	string oper;
+	cin >> oper;
+
+	cout << "Результат: " << operation(oper, a, b);
+
+
+
+}
+
